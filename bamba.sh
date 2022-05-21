b8e4b00d7cba8bbe6070cd19a230f8b17e296a35
Pastures Shadd <Pasturesshadd@gmail.com>
2022-05-21 00:25:31 +0000

    This is a test version

---
+++ bamba.sh
@@ -0,0 +1,14 @@
+
+total 24
+drwxrwxr-x. 2 ec2-user ec2-user    6 May 20 01:00 branches
+-rw-rw-r--. 1 ec2-user ec2-user   14 May 20 01:20 COMMIT_EDITMSG
+-rw-rw-r--. 1 ec2-user ec2-user  225 May 20 01:57 config
+-rw-rw-r--. 1 ec2-user ec2-user   73 May 20 01:00 description
+-rw-rw-r--. 1 ec2-user ec2-user    0 May 20 01:51 FETCH_HEAD
+-rw-rw-r--. 1 ec2-user ec2-user   23 May 20 01:00 HEAD
+drwxrwxr-x. 2 ec2-user ec2-user 4096 May 20 01:00 hooks
+-rw-rw-r--. 1 ec2-user ec2-user  209 May 20 01:20 index
+drwxrwxr-x. 2 ec2-user ec2-user   21 May 20 01:00 info
+drwxrwxr-x. 3 ec2-user ec2-user   30 May 20 01:20 logs
+drwxrwxr-x. 9 ec2-user ec2-user   80 May 20 01:51 objects
+drwxrwxr-x. 5 ec2-user ec2-user   46 May 20 02:00 refs
